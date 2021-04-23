2e875d90eb35aa127f59d51c4bf61ea926035a46
Stelios <pantelopoulosstelios@gmail.com>
2021-04-23 15:18:36 +0300

    changes DocumentParser

--- src/phase1/DocumentParser.java
+++ src/phase1/DocumentParser.java
@@ -8,6 +8,18 @@ import java.io.FileReader;
 import java.util.LinkedList;
 
 public class DocumentParser {
+
+    private static final char PREFIX = '.';
+    private static final char AUTHORS = 'A';
+    private static final char DATE = 'B';
+    private static final char CONTENT = 'C';
+    private static final char ID = 'I';
+    private static final char KEYWORDS = 'K';
+    private static final char ENTRYDATE = 'N';
+    private static final char TITLE = 'T';
+    private static final char ABSTRACT = 'W';
+    private static final char REFERENCE = 'X';
+
     private BufferedReader reader;
     private List<Document> documents;
 
@@ -21,64 +33,59 @@ public class DocumentParser {
     }
 
     public void parse() throws Exception {
-        //reads and process the documents
-        try {
-            Document document = null;
-            String line = "";
-            char state = 0;
-            while ((line = reader.readLine()) != null) {
-                if ((line = line.trim()).isEmpty()) {
-                    continue;
-                }
-                //when you find a line that starts with . (this line is attribute)
-                if (line.charAt(0) == '.') {
-                    state = line.charAt(1);
-                    if (state == 'I') {
-                        if (document != null) {
-                            documents.add(document);
-                        }
-                        document = new Document();
-                        document.setId(Integer.parseInt(line.substring(2).trim()));
-                    }
-                } else {
-                    switch (state) {
-                        case 'A':
-                            document.addAuthor(line);
-                            break;
-                        case 'D':
-                            document.addDate(line);
-                            break;
-                        case 'C':
-                            document.addContent(line);
-                            break;
-                        case 'K':
-                            for (String keyword : line.split(",")) {
-                                document.addKeywords(keyword.trim());
-                            }
-                            break;
-                        case 'N':
-                            document.addEntrydate(line);
-                            break;
-                        case 'T':
-                            document.addTitle(line);
-                            break;
-                        case 'W':
-                            document.addAbstractInfo(line);
-                            break;
-                        case 'X':
-                            document.addReference(line);
-                            break;
 
-                        case 'I':
-                        default:
+        Document document = null;
+        String line = "";
+        char state = 0;
+        while ((line = reader.readLine()) != null) {
+            if ((line = line.trim()).isEmpty()) {
+                continue;
+            }
+            //when you find a line that starts with . (this line is attribute)
+            if (line.charAt(0) == '.') {
+                state = line.charAt(1);
+                if (state == 'I') {
+                    if (document != null) {
+                       documents.add(document);
+                    }
+                    document = new Document();
+                    document.setId(Integer.parseInt(line.substring(2).trim()));
+                }
+            } else {
+                switch (state) {
+                    case AUTHORS:
+                       document.addAuthor(line);
+                        break;
+                    case DATE:
+                        document.addDate(line);
+                        break;
+                    case CONTENT:
+                        document.addContent(line);
+                        break;
+                    case KEYWORDS:
+                        for (String keyword : line.split(",")) {
+                            document.addKeywords(keyword.trim());
+                        }
+                        break;
+                    case ENTRYDATE:
+                        document.addEntrydate(line);
+                        break;
+                    case TITLE:
+                        document.addTitle(line);
+                        break;
+                    case ABSTRACT:
+                        document.addAbstractInfo(line);
+                        break;
+                    case REFERENCE:
+                        document.addReference(line);
+                        break;
+
+                    case ID:
+                    default:
                     }
                 }
             }
             documents.add(document);
             reader.close();
-
-        }catch (Throwable err) {
-            err.printStackTrace();
-        }
     }
 }
